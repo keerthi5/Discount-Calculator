9a10628477c669de5999eafed0af47d939ee3b4e
keerthi5 <31267451+keerthi5@users.noreply.github.com>
2022-03-13 22:38:12 +0530

    Add files via upload

---
+++ Helpers/IOHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDiscountCalculator.Helpers
+{
+    public class IOHelper
+    {
+        public static string GetInput(string Prompt)
+        {
+            string Result;
+            do
+            {
+                Console.Write(Prompt + " ");
+                Result = Console.ReadLine();
+                if (string.IsNullOrEmpty(Result))
+                {
+                    Console.WriteLine("Empty input. Please enter proper value again.");
+                }
+            } while (string.IsNullOrEmpty(Result));
+
+            return Result;
+        }
+    }
+}
---
+++ Helpers/LoginHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDiscountCalculator.Helpers
+{
+    class LoginHelper
+    {
+        public static string GetUserName()
+        {
+            var userName = IOHelper.GetInput("Please enter Username");
+            return userName;
+        }
+
+        public static string GetPassword()
+        {
+            var password = IOHelper.GetInput("Please enter Password");
+            return password;
+        }
+    }
+}
