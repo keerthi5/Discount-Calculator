1fc3a5eb78610ead3320edf3ed6fa3c0d3fd3f51
keerthi5 <31267451+keerthi5@users.noreply.github.com>
2022-03-13 22:41:16 +0530

    Add files via upload

---
+++ Services/LoginService.cs
@@ -0,0 +1,20 @@
+﻿using GoldDiscountCalculator.Contracts;
+using GoldDiscountCalculator.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDiscountCalculator.Services
+{
+    public class LoginService : ILoginService
+    {
+        public bool Login(string userName, string password)
+        {
+            if (DataStore.UserName == userName && DataStore.Password == password)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
