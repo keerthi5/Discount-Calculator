df5a49e674317a17b92b744732a954ccfd4ce613
keerthi5 <31267451+keerthi5@users.noreply.github.com>
2022-03-13 22:42:38 +0530

    Add files via upload

---
+++ Client.cs
@@ -0,0 +1,39 @@
+﻿using GoldDiscountCalculator.Helpers;
+using GoldDiscountCalculator.Models;
+using GoldDiscountCalculator.Services;
+using System;
+
+namespace GoldDiscountCalculator
+{
+    class Client
+    {
+        static void Main()
+        {
+            bool isLoginSuccessful;
+            do
+            {
+                string userName = LoginHelper.GetUserName();
+                string password = LoginHelper.GetPassword();
+                var loginService = new LoginService();
+                isLoginSuccessful = loginService.Login(userName, password);
+            } while (!isLoginSuccessful);
+
+            var getGoldPrice = CalculatorHelper.GetGoldValue();
+            var getGoldWeight = CalculatorHelper.GetGoldWeight();
+            var getGoldDiscount = CalculatorHelper.GetDiscount();
+
+            var goldModel = new Gold()
+            {
+                PriceVal = getGoldPrice,
+                WeightVal = getGoldWeight,
+                DiscountVal = getGoldDiscount
+            };
+
+            var calculatorService = new CalculationService();
+            var priceAfterDiscount = calculatorService.CalculatePrice(goldModel);
+
+            Console.WriteLine(priceAfterDiscount);
+        }
+    }
+}
+
