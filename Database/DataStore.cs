7925a1daa78e5c55a2ec7daa57bf28f1607b869d
keerthi5 <31267451+keerthi5@users.noreply.github.com>
2022-03-13 22:36:48 +0530

    Create DataStore.cs

---
+++ Database/DataStore.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDiscountCalculator.Database
+{
+    public class DataStore
+    {
+        public static string UserName = "User1";
+        public static string Password = "Pwd1";
+    }
+}
