120e975573f6ac5d7cb877e42190d49dec8e4b41
keerthi5 <31267451+keerthi5@users.noreply.github.com>
2022-03-13 22:39:21 +0530

    Create Gold.cs

---
+++ Models/Gold.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDiscountCalculator.Models
+{
+    public class Gold
+    {
+        public double PriceVal { get; set; }
+        public double WeightVal { get; set; }
+        public double DiscountVal { get; set; }
+    }
+}
