65f7d661b75f5237684c8b4384390e7e361eceac
keerthi5 <31267451+keerthi5@users.noreply.github.com>
2022-03-13 22:35:45 +0530

    Add files via upload

---
+++ Contracts/ILoginService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDiscountCalculator.Contracts
+{
+    interface ILoginService
+    {
+        bool Login(string userName, string password);
+    }
+}
